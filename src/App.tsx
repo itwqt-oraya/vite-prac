954d00a03e1888cc836e9e99f75e46069024a54d
Edrine05 <edrinel@qualitytrade.com>
2025-02-18 15:28:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/main.scss'
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+
+      <nav>
+        <li className='list'>sample</li>
+        <li className='list--state-green'>sample</li>
+        <li>sample</li>
+      </nav>
+    </>
+  )
+}
+
+export default App
